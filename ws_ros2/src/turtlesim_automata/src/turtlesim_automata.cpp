2de9f30f666aaf11ef2b1b3abb58ae399cc6ffe3
Lukes Ubuntu <f.luky@gmx.at>
2024-05-14 21:39:20 +0200

    Format and comments

--- ws_ros2/src/turtlesim_automata/src/turtlesim_automata.cpp
+++ ws_ros2/src/turtlesim_automata/src/turtlesim_automata.cpp
@@ -4,47 +4,54 @@
 
 class TurtleController : public rclcpp::Node {
 public:
+    // Send turtle commands periodically with a timer
+    const int cycletimeTurtleCommands_ = 100; // [ms]
+    // After edge detection we do not want to change direction in the first few cycles for a smoother ride
     int counterEdgeDetection_ = 0;
+    const int cyclesOfNoAngularChange_ = 10;
+    // Parameters
+    const int velocityTurtle_ = 2;
+    const int randomAgularVelocityRange_ = 4; // Modulo: x % y = range [0,y-1]
+
+
+
     TurtleController() : Node("turtle_controller") {
         // Create publisher to publish velocity commands
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
 
         // Create subscriber to get current pose of the turtle
-        subscriber_ = this->create_subscription<turtlesim::msg::Pose>(
-            "turtle1/pose",
-            10,
-            [this](const turtlesim::msg::Pose::SharedPtr pose) {
-                // Update the current pose of the turtle
-                current_pose_ = *pose;
-            });
+        subscriber_ = this->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10, [this](const turtlesim::msg::Pose::SharedPtr pose) {
+            // Update the current pose of the turtle
+            current_pose_ = *pose;
+        });
 	
         // Timer to publish velocity commands periodically
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(100), [this]() {
-            // Publish velocity commands to make the turtle move in a square pattern
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(cycletimeTurtleCommands_), [this]() {
+            // Publish velocity commands to make the turtle move
             geometry_msgs::msg::Twist msg;
             
-            // Edge detection
-            if ((current_pose_.x < 1 || current_pose_.x > 10 || 
-	    	current_pose_.y < 1 || current_pose_.y > 10) &&
-	    	(counterEdgeDetection_ == 0)) {
-            	// Stop linear
+            // Edge detection with counter
+            if ((current_pose_.x < 0.5 || current_pose_.x > 10.5 || current_pose_.y < 0.5 || current_pose_.y > 10.5) && (counterEdgeDetection_ == 0)) {
+            	// Stop linear when we reach the borders
             	msg.linear.x = 0;
             	// Rotate clockwise 90 degree per second (3.14/2 [rad/s])
-            	msg.angular.z = 15.7;
+            	msg.angular.z = (3.14/2)/cycletimeTurtleCommands_*1000; // 90 degree / cycletime
             	counterEdgeDetection_++;
             } else {
-            	// Move forward 1.0 [m/s]
-            	msg.linear.x = 2;
-            	// Rotate with an angular random velocity between -1 and 1 [rad/s]
+            	// Move forward
+            	msg.linear.x = velocityTurtle_;
+            	// The first few cycles after an edge detection we don not want to rotate and rather move away from the edge
             	if (counterEdgeDetection_ > 0) {
             		msg.angular.z = 0;
             		counterEdgeDetection_++;
-            		if (counterEdgeDetection_ > 10) { counterEdgeDetection_ = 0; }
+            		if (counterEdgeDetection_ > cyclesOfNoAngularChange_) { counterEdgeDetection_ = 0; } // Set counter to 0
             	} else {
+                // Otherwise we rotate with an angular random velocity between -2 and 2 [rad/s]
             	srand(time(0));
-            	msg.angular.z = (rand() % 4) - 2;
+            	msg.angular.z = (rand() % (randomAgularVelocityRange_+1)) - (randomAgularVelocityRange_/2); // Example: rand() % (4+1) = range[0..4] -> - 4/2 = range[-2..2]
             	}
             }
+            
             publisher_->publish(msg);
         });
     }
